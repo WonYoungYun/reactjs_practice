f16788e17eaaaad09e04d671639048350a9aa1e5
WonYoungYun <asmar20288@naver.com>
2018-08-20 19:07:46 +0900

    start

---
+++ hello-react/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';//js확장자는 생략해도된다
+
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App/>, rootElement);
