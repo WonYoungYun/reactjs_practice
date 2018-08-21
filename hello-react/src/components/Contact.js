35ca6dbd303697015be5dd2b413a3f2b3f97dc64
WonYoungYun <asmar20288@naver.com>
2018-08-21 16:52:50 +0900

    react

--- hello-react/src/components/Contact.js
+++ hello-react/src/components/Contact.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ContactInfo from './ContactInfo';
 import ContactDetails from './ContactDetails';
+import ContactCreate from './ContactCreate';
+import update from 'react-addons-update';
 
 export default class Contact extends React.Component{
     constructor(props) {
@@ -22,8 +24,25 @@ export default class Contact extends React.Component{
 
         this.handleChange = this.handleChange.bind(this);
         this.handleClick = this.handleClick.bind(this);
-    }
 
+        this.handleCreate = this.handleCreate.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+    }
+    componentWillMount(){
+        //돔이 생성되기전에 실행
+        const contactData = localStorage.contactData;
+        if(contactData){
+            this.setState({
+                contactData: JSON.parse(contactData)
+            });
+        }
+    }
+    componentDidUpdate(prevProps, prevState){
+        if(JSON.stringify(prevState.contactData) !== JSON.stringify(this.state.contactData)) {
+            localStorage.contactData = JSON.stringify(this.state.contactData);
+        }
+    }
     handleChange(e){
         this.setState({
             keyword: e.target.value
@@ -35,6 +54,33 @@ export default class Contact extends React.Component{
         });
         console.log(key);
     }
+    handleCreate(contact){
+        this.setState({
+            contactData: update(this.state.contactData, {$push:[contact]})
+        });
+    }
+    handleRemove(){
+        if(this.state.selectedKey <0){
+            return;
+        }
+        this.setState({
+            contactData: update(this.state.contactData, {$splice:[[this.state.selectedKey, 1]]}
+            ),
+            selectedKey: -1
+        })
+    }
+    handleEdit(name, phone){
+        this.setState({
+            contactData: update(this.state.contactData,{
+                [this.state.selectedKey]: {
+                    name: {$set:name},
+                    phone: {$set:phone}
+                }
+            })
+        });
+    }
+
+
         render(){
       
             const mapToComponent = (data) => {
@@ -65,6 +111,11 @@ export default class Contact extends React.Component{
                 <div>{mapToComponent(this.state.contactData)}</div>
                 <ContactDetails isSelected={this.state.selectedKey !== -1}
                     contact={this.state.contactData[this.state.selectedKey]}
+                    onRemove={this.handleRemove}
+                    onEdit={this.handleEdit}
+                />
+                <ContactCreate
+                    onCreate={this.handleCreate}
                 />
               </div>
             );
