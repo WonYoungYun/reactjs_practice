890ed7b67d533823bd281cd4f227cffb3af926b4
WonYoungYun <asmar20288@naver.com>
2018-08-23 17:26:18 +0900

    movie

--- Area2/super_cool/src/App.js
+++ Area2/super_cool/src/App.js
@@ -1,12 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Movie from './Movie'
+
+import Transformer from './images/Transformer.png';
+import Alog_with_the_gods from './images/Along with the gods.png';
+import Equalibrium from './images/Equalibrium.png';
+import Your_name from './images/Your name.png';
+
 
 class App extends Component {
+  state = {
+    greeting: 'Hello',
+    movies : [
+      {
+        title: "Transformer",
+        poster: Transformer
+      },
+      {
+        title: "Alog wit the gods",
+        poster: Alog_with_the_gods
+      },
+      {
+        title: "Equalibrium",
+        poster: Equalibrium
+      },
+      {
+        title: "Your name",
+        poster: Your_name
+      }
+    ]
+  }
+
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({
+        greeting: "Hello again!"
+      })
+    }, 5000)
+  }
+
   render() {
     return (
       <div className="App">
-
+        {this.state.greeting}
+        {this.state.movies.map((movie, index) => {
+          return <Movie title={movie.title} poster={movie.poster} key={index}/>
+          
+        })}
       </div>
     );
   }
